ee4ab6a768e863d7e17c285d20e2f374613aebb8
Bipasha Chakraborty <86914435+BIPASHA-CHAKRABORTY@users.noreply.github.com>
2024-11-19 16:35:22 +0530

    Create DAY-1.cpp

---
+++ DAY-1.cpp
@@ -0,0 +1,15 @@
+// USING CPP FOR DSA
+//REVISING CPP
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a,b;
+  cout<<"Enter value of a";
+  cin>>a;
+  cout<<"Enter value of b";
+  cin>>b;
+
+return 0;
+}
